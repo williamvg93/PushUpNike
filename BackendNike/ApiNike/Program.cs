2ec574b34b0e1b3c6a130a458f924dc82abcfc7c
williamvg93 <sonico_5071@hotmail.com>
2023-12-14 11:58:13 -0500

    feat: :sparkles: The ApplicationServiceExtensions was added, the connectionString was added in the app.Settings
    The Application Service Extensions was added, the connectionString was added in the app.Settings, the AppDbContext was added in the program

--- BackendNike/ApiNike/Program.cs
+++ BackendNike/ApiNike/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+/* Add connection to database */
+builder.Services.AddDbContext<ApiNikeContext>(options =>
+{
+    string connectionStrings = builder.Configuration.GetConnectionString("MysqlConect");
+    options.UseMySql(connectionStrings, ServerVersion.AutoDetect(connectionStrings));
+});
 
 var app = builder.Build();
 
@@ -16,35 +28,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
 
 app.Run();
 
-
-/* dotnet ef dbcontext scaffold "server=localhost;database=nike;user=root;password=campus2024" Pomelo.Entity.FrameworkCore.MySql -s ApiNike -p Domain --context ApiNikeContext --context-dir Data --output-dir Entities */
-
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 
